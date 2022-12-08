f773b345baee26336500500141b53cb3f658b92d
thanhtu <ntu34543@gmail.com>
2022-12-08 22:24:30 +0700

    first commit

---
+++ .history/App_20221208194627.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {ActivityIndicator, StyleSheet} from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistor} from './src/features/store';
+import {store} from './src/features/store';
+import Home from './src/screens/Home';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
+        <Home />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
